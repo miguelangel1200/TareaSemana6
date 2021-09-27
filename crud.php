844cd603325c1c3c35aa53aa0868a6afe72f5139
miguelangel1200 <75793818+miguelangel1200@users.noreply.github.com>
2021-09-27 01:17:59 -0500

    Add files via upload

---
+++ crud.php
@@ -0,0 +1,36 @@
+<?php
+include_once 'db.php';
+
+if(isset($_POST['save']))
+{
+    $fn = $MySQLiconn->real_escape_string($_POST['fn']);
+    $ln = $MySQLiconn->real_escape_string($_POST['ln']);
+
+    $SQL = $MySQLiconn->query("INSERT INTO data(fn,ln) VALUES('$fn','$ln')");
+
+
+    if(!$SQL)
+    {
+        echo $MySQLiconn->error;
+    }
+}
+
+if(isset($_GET['del']))
+{
+    $SQL = $MySQLiconn->query("DELETE FROM data WHERE id=".$_GET['del']);
+    header("Location: index_5.php");
+}
+
+if(isset($_GET['edit'])){
+    
+    $SQL=$MySQLiconn->query("SELECT * FROM data WHERE id=".$_GET['edit']);
+
+    $getRow=$SQL->fetch_array();  
+}
+
+if(isset($_POST['update'])){
+
+    $SQL=$MySQLiconn->query("UPDATE data SET fn='".$_POST['fn']."', ln='".$_POST['ln']."' WHERE id=".$_GET['edit']);
+    
+    header("Location: index_5.php");
+}
